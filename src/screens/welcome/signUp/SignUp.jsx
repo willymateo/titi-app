3146e7db1eb5160f0a2e37337dfebfb0596e4ff0
willymateo <wjmateo@espol.edu.ec>
2022-08-06 09:40:11 -0500

    InputChip component created

--- src/screens/welcome/signUp/SignUp.jsx
+++ src/screens/welcome/signUp/SignUp.jsx
@@ -1,4 +1,4 @@
-import { Button, Chip, Dialog, Portal, RadioButton, Text, TextInput } from "react-native-paper";
+import { Button, Dialog, Portal, RadioButton, TextInput } from "react-native-paper";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
 import { RadioButtonGroupHF } from "../../../components/RadioButtonGroupHF";
@@ -6,6 +6,7 @@ import { TextInputHookForm } from "../../../components/TextInputHookForm";
 import { EMAIL_REGEX, USERNAME_REGEX } from "../../../share/app.config";
 import { LoginFooter } from "../../../components/LoginFooter";
 import { ScrollView, StyleSheet, View } from "react-native";
+import { InputChip } from "../../../components/InputChip";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import {
@@ -47,122 +48,119 @@ function SignUp({ navigation }) {
       showsVerticalScrollIndicator={false}
       contentContainerStyle={styles.container}>
       <View>
-        <TextInputHookForm
-          rules={{
-            required: "Email is required",
-            pattern: {
-              value: EMAIL_REGEX,
-              message: "Email is invalid",
-            },
-          }}
-          label="Email"
-          control={control}
-          controllerName="email"
-          style={styles.inputText}
-          left={<TextInput.Icon name={props => <Mail {...props} {...styles.iconoir} />} />}
-        />
-        <TextInputHookForm
-          rules={{
-            required: "Username is required",
-            minLength: {
-              value: 5,
-              message: "Username should be minimum 5 characters long",
-            },
-            maxLength: {
-              value: 30,
-              message: "Username should be maximum 30 characters long",
-            },
-            pattern: {
-              value: USERNAME_REGEX,
-              message: `Username should be in lowercase.
+        <View>
+          <TextInputHookForm
+            rules={{
+              required: "Email is required",
+              pattern: {
+                value: EMAIL_REGEX,
+                message: "Email is invalid",
+              },
+            }}
+            label="Email"
+            control={control}
+            controllerName="email"
+            style={styles.inputText}
+            left={<TextInput.Icon name={props => <Mail {...props} {...styles.iconoir} />} />}
+          />
+          <TextInputHookForm
+            rules={{
+              required: "Username is required",
+              minLength: {
+                value: 5,
+                message: "Username should be minimum 5 characters long",
+              },
+              maxLength: {
+                value: 30,
+                message: "Username should be maximum 30 characters long",
+              },
+              pattern: {
+                value: USERNAME_REGEX,
+                message: `Username should be in lowercase.
 The only allowed special characters are '_' and '.'`,
-            },
-          }}
-          label="Username"
-          control={control}
-          style={styles.inputText}
-          controllerName="username"
-          left={<TextInput.Icon name={props => <User {...props} {...styles.iconoir} />} />}
-        />
-        <TextInputHookForm
-          label="Password"
-          control={control}
-          style={styles.inputText}
-          controllerName="password"
-          secureTextEntry={isPasswordHidden}
-          rules={{ required: "Password is required" }}
-          left={<TextInput.Icon name={props => <KeyAlt {...props} {...styles.iconoir} />} />}
-          right={
-            <TextInput.Icon
-              name={props => {
-                return isPasswordHidden ? (
-                  <EyeClose {...props} {...styles.iconoir} />
-                ) : (
-                  <EyeEmpty {...props} {...styles.iconoir} />
-                );
-              }}
-              onPress={() => setIsPasswordHidden(!isPasswordHidden)}
-            />
-          }
-        />
-        <TextInputHookForm
-          rules={{
-            required: "Password is required",
-            validate: value => value === password || "Password do not match",
-          }}
-          secureTextEntry
-          control={control}
-          label="Repeat password"
-          style={styles.inputText}
-          controllerName="repeatPassword"
-          left={<TextInput.Icon name={props => <KeyAltBack {...props} {...styles.iconoir} />} />}
-        />
+              },
+            }}
+            label="Username"
+            control={control}
+            style={styles.inputText}
+            controllerName="username"
+            left={<TextInput.Icon name={props => <User {...props} {...styles.iconoir} />} />}
+          />
+          <TextInputHookForm
+            label="Password"
+            control={control}
+            style={styles.inputText}
+            controllerName="password"
+            secureTextEntry={isPasswordHidden}
+            rules={{ required: "Password is required" }}
+            left={<TextInput.Icon name={props => <KeyAlt {...props} {...styles.iconoir} />} />}
+            right={
+              <TextInput.Icon
+                name={props => {
+                  return isPasswordHidden ? (
+                    <EyeClose {...props} {...styles.iconoir} />
+                  ) : (
+                    <EyeEmpty {...props} {...styles.iconoir} />
+                  );
+                }}
+                onPress={() => setIsPasswordHidden(!isPasswordHidden)}
+              />
+            }
+          />
+          <TextInputHookForm
+            rules={{
+              required: "Password is required",
+              validate: value => value === password || "Password do not match",
+            }}
+            secureTextEntry
+            control={control}
+            label="Repeat password"
+            style={styles.inputText}
+            controllerName="repeatPassword"
+            left={<TextInput.Icon name={props => <KeyAltBack {...props} {...styles.iconoir} />} />}
+          />
 
-        <View style={styles.chipsRow}>
-          <View style={styles.chipInput}>
-            <Text style={styles.labelChip}>Born date</Text>
-            <Chip
-              mode="flat"
-              onPress={showDatePicker}
-              icon={props => <Calendar {...props} {...styles.iconoir} />}>
-              {bornDate.toLocaleDateString()}
-            </Chip>
-          </View>
+          <InputChip
+            mode="flat"
+            Icon={Calendar}
+            label="Born date"
+            style={styles.inputChip}
+            onPress={showDatePicker}
+            value={bornDate.toLocaleString()}
+          />
+          <InputChip
+            mode="flat"
+            label="Genre"
+            Icon={PeopleRounded}
+            value={"not specified"}
+            style={styles.inputChip}
+            onPress={() => setVisibleDialog(true)}
+          />
 
-          <View style={styles.chipInput}>
-            <Text style={styles.labelChip}>Genre</Text>
-            <Chip
-              mode="flat"
-              onPress={() => setVisibleDialog(true)}
-              icon={props => <PeopleRounded {...props} {...styles.iconoir} />}>
-              not specified
-            </Chip>
-          </View>
+          <Portal>
+            <Dialog visible={visibleDialog} onDismiss={() => setVisibleDialog(false)}>
+              {/* <Dialog.Icon icon={props => <EmojiBlinkRight {...props} {...styles.iconoir} />} />*/}
+              <Dialog.Title style={styles.dialogTitle}>Genre</Dialog.Title>
+              <Dialog.ScrollArea>
+                <ScrollView>
+                  <RadioButtonGroupHF
+                    control={control}
+                    controllerName="idGenre"
+                    rules={{ required: "Genre is required" }}>
+                    {genres.map(({ id, genre }) => (
+                      <RadioButton.Item label={genre} value={id} key={id} />
+                    ))}
+                  </RadioButtonGroupHF>
+                </ScrollView>
+              </Dialog.ScrollArea>
+              <Dialog.Actions>
+                <Button onPress={() => setVisibleDialog(false)} uppercase={false}>
+                  Close
+                </Button>
+              </Dialog.Actions>
+            </Dialog>
+          </Portal>
         </View>
-
-        <Portal>
-          <Dialog visible={visibleDialog} onDismiss={() => setVisibleDialog(false)}>
-            {/* <Dialog.Icon icon={props => <EmojiBlinkRight {...props} {...styles.iconoir} />} />*/}
-            <Dialog.Title style={styles.dialogTitle}>Genre</Dialog.Title>
-            <Dialog.ScrollArea>
-              <ScrollView>
-                <RadioButtonGroupHF
-                  control={control}
-                  controllerName="idGenre"
-                  rules={{ required: "Genre is required" }}>
-                  {genres.map(({ id, genre }) => (
-                    <RadioButton.Item label={genre} value={id} key={id} />
-                  ))}
-                </RadioButtonGroupHF>
-              </ScrollView>
-            </Dialog.ScrollArea>
-            <Dialog.Actions>
-              <Button onPress={() => setVisibleDialog(false)} uppercase={false}>
-                Close
-              </Button>
-            </Dialog.Actions>
-          </Dialog>
-        </Portal>
 
         {/*
         <TextInputHookForm
@@ -177,13 +175,15 @@ The only allowed special characters are '_' and '.'`,
         />
           */}
 
-        <Button mode="contained" uppercase={false} onPress={handleSubmit(onPressContinue)}>
-          Continue
-        </Button>
-        <LoginFooter
-          onPressLogin={() => navigation.navigate("Login")}
-          onPressAccountRecovery={() => navigation.navigate("AccountRecovery")}
-        />
+        <View>
+          <Button mode="contained" uppercase={false} onPress={handleSubmit(onPressContinue)}>
+            Continue
+          </Button>
+          <LoginFooter
+            onPressLogin={() => navigation.navigate("Login")}
+            onPressAccountRecovery={() => navigation.navigate("AccountRecovery")}
+          />
+        </View>
       </View>
     </KeyboardAwareScrollView>
   );
@@ -207,19 +207,8 @@ const styles = StyleSheet.create({
   dialogTitle: {
     textAlign: "center",
   },
-  chipInput: {
-    paddingHorizontal: 10,
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  labelChip: {
-    marginRight: 5,
-  },
-  chipsRow: {
-    marginVertical: 20,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+  inputChip: {
+    marginVertical: 12,
   },
 });
 
